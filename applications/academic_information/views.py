ac8db2e3de1c1524a029b715d27490645ba189a2
Vivek Shukla <vivekshukla@iiitdmj.ac.in>
2017-10-28 18:47:50 +0530

    added attendance in views.py

--- applications/academic_information/views.py
+++ applications/academic_information/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
 from django.shortcuts import render
 from .models import Student_attendance,Course,Student
-from django.http import HttpResponseRedirect
+
+from django.http import HttpResponseRedirect,HttpResponse
 
 def homepage(request):
     context = {}
@@ -10,12 +11,14 @@ def homepage(request):
 
 def add_attendance(request):
     student_attend=Student_attendance()
-    student_attend.student_id=request.POST.get('student_id')
+    s_id=request.POST.get('student_id')
+    c_id=request.POST.get('course_id')
+    student_attend.student_id=Student.objects.get(id_id=s_id)
     print('student_id ',student_attend.student_id)
-    student_attend.course_id=request.POST.get('course_id')
+    student_attend.course_id=Course.objects.get(course_id=c_id)
     student_attend.present_attend=request.POST.get('present_attend')
     student_attend.total_attend=request.POST.get('total_attend')
     student_attend.save()
-    return HttpResponseRedirect('')
+    return HttpResponse("<h1>Data Added<h1>")
 
 
