18e219b4cf78f17e17f87104f9660bc647f52024
Vivek Shukla <vivekshukla@iiitdmj.ac.in>
2017-10-28 14:12:12 +0530

    modified attendance class model and views

--- applications/academic_information/views.py
+++ applications/academic_information/views.py
@@ -1,10 +1,21 @@
 # from django.shortcuts import render
-
 from django.shortcuts import render
-
+from .models import Student_attendance,Course,Student
+from django.http import HttpResponseRedirect
 
 def homepage(request):
     context = {}
 
     return render(request, "ais/ais.html", context)
+
+def add_attendance(request):
+    student_attend=Student_attendance()
+    student_attend.student_id=request.POST.get('student_id')
+    print('student_id ',student_attend.student_id)
+    student_attend.course_id=request.POST.get('course_id')
+    student_attend.present_attend=request.POST.get('present_attend')
+    student_attend.total_attend=request.POST.get('total_attend')
+    student_attend.save()
+    return HttpResponseRedirect('')
+
 
